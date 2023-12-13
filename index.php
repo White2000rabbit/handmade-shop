5eb533e0a3ab5eb137902cb5ebf3b9703cfd6515
Alyona git config --global user.email alena_novour@mail.ru <alena_novour@mail.ru>
2023-12-13 22:23:34 +0300

    refactor

--- index.php
+++
@@ -1,7 +0,0 @@
-<?php
-   require("blocks/header.php");
-   require("blocks/popular-product.php");
-   require("blocks/modal-popup.php");
-   require("blocks/arrow-top.php");
-   require("blocks/footer.php");
-?>
