a021dfedf5ac79104e398a3b7ce3e334b96cd354
Alyona git config --global user.email alena_novour@mail.ru <alena_novour@mail.ru>
2023-12-14 21:16:40 +0300

    feat: add js

---
+++ js/const.js
@@ -0,0 +1,38 @@
+export const CategoryType = {
+   ALL: 'all',
+   HOME: 'home',
+   CHRISTMASS: 'christmas',
+   ACCESSORIES: 'accessories',
+   PAINTINGS: 'paintings',
+   };
+
+export const categories = [
+   {
+   name: CategoryType.ALL,
+   title: 'Все',
+   },
+   {
+   name: CategoryType.HOME,
+   title: 'Недвижимость',
+   },
+   {
+   name: CategoryType.CHRISTMASS,
+   title: 'Елочные игрушки',
+   },
+   {
+   name: CategoryType.ACCESSORIES,
+   title: 'Украшения',
+   },
+   {
+   name: CategoryType.PAINTINGS,
+   title: 'Интерьерные картины',
+   },
+];
+/*
+export const UpdateType = {
+   PATCH: 'PATCH',
+   MINOR: 'MINOR',
+   MAJOR: 'MAJOR',
+   INIT: 'INIT',
+};
+*/
--- js/js.js
+++ js/js.js
@@ -1,3 +1,61 @@
+import {CategoryType} from 'const.js';
+
+const createRangeFilterTemplate = () => `<div></div>`;
+
+export class FilterAllView extends AbstractFilterView {
+   getTemplate() {
+      return createRangeFilterTemplate();
+   }
+}
+
+//фильтр категории
+export const getCategoryProducts = (products, category) => {
+   return products.filter((product) => product.category === category);
+};
+ //фильтр по цене
+export const getProductsPriceRanges = (products) => {
+   const prices = products.map((product) => product.price);
+
+   return {
+      min: Math.min.apply(null, prices),
+      max: Math.max.apply(null, prices),
+   };
+};
+//выбранная категория
+const checkCategory = (category, product) => {
+   return category === CategoryType.ALL || product.category === category;
+};
+
+const checkPrice = (filters, product) => (
+   (!filters.minPrice || product.price >= filters.minPrice) &&
+   (!filters.maxPrice || product.price <= filters.maxPrice)
+);
+
+export const filterProducts = (products, category, filters) => products.filter((product) => {
+   if (!(product.filters && checkCategory(category, product) && checkPrice(filters, product))) {
+      return false;
+   }
+switch (category) {
+   case CategoryType.HOME:
+   return (
+      checkhomeType(filters[`home-type`], product.filters.type)
+   );
+   case CategoryType.CHRISTMASS:
+   return (
+      checkchristmassType(filters[`christmass-type`], product.filters.type)
+   );
+   case CategoryType.ACCESSORIES:
+   return (
+      checkaccessoriesType(filters[`accessories-type`], product.filters.type) 
+   );
+   case CategoryType.PAINTINGS:
+   return (
+      checkpaintingsType(filters[`paintings-type`], product.filters[`paintings-type`])
+   );
+   default:
+   return true;
+   }
+});
 
 
 //Бургер-меню
