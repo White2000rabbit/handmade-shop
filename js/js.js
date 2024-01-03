e25fba33d8eba73c7f309dd3bce75bc2e848d513
Alyona git config --global user.email alena_novour@mail.ru <alena_novour@mail.ru>
2024-01-03 20:39:13 +0300

    feat: parallax

--- js/js.js
+++ js/js.js
@@ -1,3 +1,73 @@
+'use strict'
+window.onload = function () {
+
+//Параллакс 
+const parallax = document.querySelector('.parallax');
+if (parallax) {
+   const content = document.querySelector('.parallax__container');
+   const frame = document.querySelector('.parallax-img__frame');
+   const ocean = document.querySelector('.parallax-img__ocean');
+   const palm = document.querySelector('.parallax-img__palm');
+   //коэффициенты
+   const forFrame = 10;
+   const forOcean = 30;
+   const forPalm = 20;
+   //скорость анимации
+   const speed = 0.5;
+
+   let positionX = 0, positionY = 0;
+   let coordXprocent = 0, coordYprocent = 0;
+
+   function setParallaxStyle() {
+      const distX = coordXprocent - positionX;
+      const distY = coordYprocent - positionY;
+
+      positionX = positionX + (distX * speed);
+      positionY = positionY + (distY * speed);
+
+      //передаем стили объекта
+      frame.style.cssText = `transform: translate(${positionX / forFrame}%,${positionY / forFrame}%);`;
+      ocean.style.cssText = `transform: translate(${positionX / forOcean}%,${positionY / forOcean}%);`;
+      palm.style.cssText = `transform: translate(${positionX / forPalm}%,${positionY / forPalm}%);`;
+   
+      requestAnimationFrame(setParallaxStyle);
+   }
+   setParallaxStyle();
+   parallax.addEventListener('mousemove', function (e) {
+      const parallaxWidth = parallax.offsetWidth;
+      const parallaxHeight = parallax.offsetHeight;
+      //0 по середине
+      const coordX = e.pageX - parallaxWidth / 2;
+      const coordY = e.pageY - parallaxHeight / 2; 
+      //получаем процентные значения
+      coordXprocent = coordX / parallaxWidth * 100;
+      coordYprocent = cordY / parallaxHeight * 100;
+   });
+
+   //параллакс при скролле
+   let thresholdSets = [];
+   for (let i = 0; i <= 1.0; i += 0.005) {
+      thresholdSets.push(i);
+   }
+   const callback = function (entries, observer) {
+      const scrollTopProcent = window.pageYOffset / parallax.offsetHeight * 100;
+      setParallaxItemsStyle(scrollTopProcent);
+   };
+   const observer = new IntersectionObserver (callback, {
+      threshold: thresholdSets
+   });
+
+   observer.observe(document.querySelector('.content'));
+
+   function setParallaxItemsStyle(scrollTopProcent) {
+      content.style.cssText = `transform: translate(0%, -${scrollTopProcent / 0.5}%);`;
+      ocean.parentElement.style.cssText = `transform: translate(0%, -${scrollTopProcent / 4}%);`;
+      palm.parentElement.style.cssText = `transform: translate(0%, -${scrollTopProcent / 2}%);`;
+   
+   }
+}
+
+
 
 //ВЕРХНИЙ ФИЛЬТР
 const filters = document.querySelectorAll('a[data-filter]');
@@ -102,3 +172,4 @@ document.addEventListener('click', event => {
   overlay.classList.add('is-visible');
 });
 */
+}
